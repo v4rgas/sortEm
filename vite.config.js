7478e2e55ba7456b89344d8cf4cf08f519c9a9c0
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-08-19 00:55:57 -0400

    refactor: Update vite.config.js to include rollupOptions for main and 404 inputs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: '404.html'
+      }
+    }
+  },
   base: '/sortEm'
 })
