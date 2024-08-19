7d7aa988ac7a327e4afd900a28891913d63cec0c
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-08-19 13:09:33 -0400

    refactor: Update LeaderboardTable CSS for player colors and padding

--- src/Leaderboard/LeaderboardTable.jsx
+++ src/Leaderboard/LeaderboardTable.jsx
@@ -10,10 +10,11 @@ export default function LeaderboardTable({ dataToDisplay }) {
             </thead>
             <tbody>
                 {dataToDisplay?.map((obj, i) => {
+                    const tdClass = i < 3 ? `winner-td` : '';
                     return (
                         <tr key={i} className={i < 3 ? `player-${i}` : 'player-regular'} >
-                            <td>{obj.username}</td>
-                            <td>{(obj.time / 1000).toFixed(3)}</td>
+                            <td className={tdClass}>{obj.username}</td>
+                            <td className={tdClass}>{(obj.time / 1000).toFixed(3)}</td>
                         </tr>
                     );
                 })}
