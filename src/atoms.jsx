9847a42d6869f7d7eb0a5feddaf941014b672e58
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-08-20 09:33:37 -0400

    feat: Update themeAtom to use window.matchMedia for initial theme detection

--- src/atoms.jsx
+++ src/atoms.jsx
@@ -2,4 +2,4 @@ import { atomWithStorage } from "jotai/utils";
 
 export const usernameAtom = atomWithStorage("username", "");
 export const userTimesAtom = atomWithStorage("userTimes", []);
-export const themeAtom = atomWithStorage("theme", "");
+export const themeAtom = atomWithStorage("themeStorage", window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
