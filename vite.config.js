9258a1f1618e221cae370920650953ea18803795
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-08-24 00:41:32 -0400

    feat: Update PWA assets and manifest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,44 @@
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SortEm',
+        short_name: 'sortEm',
+        theme_color: '#ebebeb',
+        icons: [
+            {
+                src: 'pwa-64x64.png',
+                sizes: '64x64',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
+        ],
+      }, 
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
